efcc1e3fcb98accc4df47a3570bc1b7db0875e36
Farrel Abennaya <114734387+farrelabennaya@users.noreply.github.com>
2022-12-08 12:28:10 +0700

    modul 4

---
+++ Modul_4/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_4;
+
+/**
+ *
+ * @author Abennaya
+ */
+public class Hello
+{
+ /**
+ * My first java program
+ */
+ public static void main(String[] args) {
+ //menampilkan string”Hello world” pada screen
+
+ System.out.println("Hello world!");
+ }
+} 
