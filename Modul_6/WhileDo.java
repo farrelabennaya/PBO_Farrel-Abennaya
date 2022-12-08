f83a96e548afd3487f4d9435c8b9c1d654f6c616
Farrel Abennaya <114734387+farrelabennaya@users.noreply.github.com>
2022-12-08 12:30:49 +0700

    Add files via upload

---
+++ Modul_6/WhileDo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_6;
+
+/**
+ *
+ * @author Abennaya
+ */
+public class WhileDo {
+ public static void main(String args[]){
+ int r = 0;
+ do {
+  System.out.println("Farrel Abennaya");
+  r++;
+  } while (r<100);
+ }
+}
