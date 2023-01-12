1d7c72284d806614d35f6bf3d210cf7dfa0e7d5f
Farrel Abennaya <114734387+farrelabennaya@users.noreply.github.com>
2023-01-12 23:44:55 +0700

    Add files via upload

---
+++ Modul_11/Employes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_11;
+
+/**
+ *
+ * @author Abennaya
+ */
+public class Employes extends Person{
+    public String getName(){ 
+        System.out.println("getName Employes");
+        return name; 
+    }
+}
